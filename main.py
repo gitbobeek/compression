1b2fe8506a7af14e3d3aa89e4cfdcc5c24f543ce
grizz <grizzlyenot@gmail.com>
2025-02-12 16:29:30 +0300

    Part 1

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+def run_length_encode(data: bytes, outf) -> None:
+    if len(data) == 0:
+        return
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    counter = 1
+    for i in range(1, len(data)):
+        if data[i-1] == data[i]:
+            counter += 1
+        else:
+            outf.write(hex(counter).encode('ascii') + b' ' + hex(data[i-1]).encode('utf-8') + b' ')
+            counter = 1
+    outf.write(hex(counter).encode('ascii') + b' ' + hex(data[i-1]).encode('utf-8') + b' ')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def decode(data: bytes) -> bytes:
+    decoded = bytearray()
+    text_bytes = data.split(b' ')
+    i = 0
+    while i < len(text_bytes) - 1:
+        count = int(text_bytes[i], 16)
+        byte = int(text_bytes[i + 1], 16)
+        decoded.extend([byte] * count)
+        i += 2
+    return bytes(decoded)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_encoded_size(data: str) -> int:
+    counter = 0
+    for i in data:
+        if i == ' ':
+            counter += 1
+    return counter
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+f = open('test_input_file', 'rb').read()
+original_size = len(f)
+
+out = open('test_output_file', 'wb')
+run_length_encode(f, out)
+out.close()
+
+encoded_data = open('test_output_file', 'rb').read()
+encoded_size = get_encoded_size(str(encoded_data))
+
+decoded_data = decode(encoded_data)
+
+out = open('test_decoded_file', 'wb')
+out.write(decoded_data)
+out.close()
+
+compression_ratio = original_size / encoded_size if encoded_size != 0 else 0
+print(f"Original size: {original_size} bytes")
+print(f"Encoded size: {encoded_size} bytes")
+print(f"Compression ratio: {compression_ratio:.2f}")
