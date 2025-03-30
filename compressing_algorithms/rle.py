a24bf0f1782067593feb15e9b6da11b14f501c4b
gitbobeek <grizzlyenot@gmail.com>
2025-03-30 14:29:39 +0300

    RLE теперь содержит флаговый бит в управляющем байте

--- compressing_algorithms/rle.py
+++ compressing_algorithms/rle.py
@@ -3,7 +3,7 @@ def rle_compress(data: bytes) -> bytes:
     i = 0
     while i < len(data):
         run_length = 1
-        while i + run_length < len(data) and data[i] == data[i + run_length] and run_length < 255:
+        while i + run_length < len(data) and data[i] == data[i + run_length] and run_length < 127:
             run_length += 1
 
         if run_length > 1:
@@ -13,13 +13,12 @@ def rle_compress(data: bytes) -> bytes:
         else:
             non_repeat_length = 1
             while (i + non_repeat_length < len(data) and
-                   non_repeat_length < 255 and
+                   non_repeat_length < 127 and
                    (i + non_repeat_length + 1 >= len(data) or
                     data[i + non_repeat_length] != data[i + non_repeat_length + 1])):
                 non_repeat_length += 1
 
-            encoded.append(non_repeat_length)
-            encoded.append(0x80)
+            encoded.append(non_repeat_length | 0x80)
             encoded.extend(data[i:i + non_repeat_length])
             i += non_repeat_length
 
@@ -30,12 +29,12 @@ def rle_decompress(data: bytes) -> bytes:
     decoded = []
     i = 0
     while i < len(data):
-        run_length = data[i]
-        if i + 1 < len(data) and data[i + 1] == 0x80:
-            length = run_length
-            decoded.extend(data[i + 2:i + 2 + length])
-            i += 2 + length
+        length_byte = data[i]
+        if length_byte & 0x80:
+            length = length_byte & 0x7F
+            decoded.extend(data[i + 1:i + 1 + length])
+            i += 1 + length
         else:
-            decoded.extend([data[i + 1]] * run_length)
+            decoded.extend([data[i + 1]] * length_byte)
             i += 2
     return bytes(decoded)
